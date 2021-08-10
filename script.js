61954ef6ea68f3000ba6365985fcb4580deba497
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-10 15:57:43 +0400

    feat: заменил задний фон и исправил заголовок

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const books = document.querySelectorAll('.books');
 const book = document.querySelectorAll('.book');
+const h2 = document.querySelectorAll('h2');
 
 book[1].remove();
 book[2].remove();
@@ -11,3 +12,9 @@ books[0].append(book[3]);
 books[0].append(book[5]);
 book[5].after(book[2]);
 books[0].prepend(book[1]);
+
+//Заменить картинку заднего фона 
+document.body.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
+
+//Исправить заголовок в книге 3
+h2[4].innerHTML = '<a>Книга 3. this и Прототипы Объектов</a>';
