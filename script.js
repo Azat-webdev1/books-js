541c2045a9b89064a0c089227ef567be6598c889
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-10 16:05:51 +0400

    feat: восстановил порядок во 2 и 5 книге

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const books = document.querySelectorAll('.books');
 const book = document.querySelectorAll('.book');
 const h2 = document.querySelectorAll('h2');
+const adv = document.querySelector('.adv');
+const blanks = document.querySelectorAll('ul');
+const blank = document.querySelectorAll('li');
 
 book[1].remove();
 book[2].remove();
@@ -18,3 +21,14 @@ document.body.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
 
 //Исправить заголовок в книге 3
 h2[4].innerHTML = '<a>Книга 3. this и Прототипы Объектов</a>';
+
+adv.remove();
+
+//Восстановить порядок глав во второй и пятой книге
+blank[9].after(blank[2]);
+blank[3].after(blank[6]);
+blank[9].before(blank[7]);
+blank[4].before(blank[8]);
+blank[49].before(blank[55]);
+blank[50].after(blank[48]);
+blank[54].before(blank[51]);
